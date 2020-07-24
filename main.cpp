0ae1638a9d92b2883a52578a0559adc1ecf9988e
Kevin <kevin.kevinleo@gmail.com>
2020-07-24 17:54:05 +1000

    Walk all items (except IncludeI and output)

--- main.cpp
+++ main.cpp
@@ -15,25 +15,67 @@ using std::vector;
 struct UniquePrinter {
   std::unordered_set<string> seen;
 
-  void print(Expression* e) {
-    std::stringstream ss;
-    MiniZinc::Printer p(ss, 0, true);
-    p.print(e);
-    string s = ss.str();
+  void print(std::string& s) {
     if(seen.find(s) == seen.end()) {
       seen.insert(s);
       std::cout << s << "\n";
     }
   }
+  void print(Expression* e) {
+    std::stringstream ss;
+    MiniZinc::Printer p(ss, 0, true);
+    p.print(e);
+    std::string s = ss.str();
+    print(s);
+  }
 };
 
-struct ExpressionExtractor : public EVisitor {
-    UniquePrinter p;
+struct ExpressionExtractorEVisitor : public EVisitor {
+    UniquePrinter& p;
+
+    ExpressionExtractorEVisitor(UniquePrinter& p1) : p{p1} {}
 
     bool enter(Expression* e) {
       p.print(e);
       return true;
     }
+};
+
+struct ExpressionExtractor : public ItemVisitor {
+  UniquePrinter p;
+  ExpressionExtractorEVisitor eev;
+  ExpressionExtractor() : eev{p} {}
+
+
+  bool enter(Item* item) {
+    return !item->isa<IncludeI>();
+  }
+  void vVarDeclI(VarDeclI* vdi) {
+    VarDecl* vd = vdi->e();
+    topDown(eev, vd);
+  }
+  void vAssignI(AssignI* ai) {
+    p.print(ai->id().str());
+    topDown(eev, ai->decl());
+    topDown(eev, ai->e());
+  }
+  void vConstraintI(ConstraintI* ci) {
+    topDown(eev, ci->e());
+  }
+  void vSolveI(SolveI* si) {
+    topDown(eev, si->e());
+  }
+  void vFunctionI(FunctionI* fi) {
+    p.print(fi->id().str());
+    topDown(eev, fi->ti());
+    topDown(eev, fi->e());
+
+    auto& params = fi->params();
+    for(unsigned int i=0; i<params.size(); i++) {
+      topDown(eev, params[i]);
+    }
+
+  }
 };
 
 int main(int argc, char**argv) {
@@ -51,16 +93,8 @@ int main(int argc, char**argv) {
   Model* m = MiniZinc::parse(env, {mzn_path}, {}, "", "", includes, false, false, false, std::cerr);
 
   ExpressionExtractor ee;
-  for (MiniZinc::VarDeclIterator it = m->begin_vardecls(); it != m->end_vardecls(); ++it) {
-    VarDecl* vd = it->e();
-    topDown(ee, vd->id());
-    topDown(ee, vd->ti());
-    topDown(ee, vd->e());
-  }
-  for (MiniZinc::ConstraintIterator it = m->begin_constraints(); it != m->end_constraints(); ++it) {
-    Expression* e = it->e();
-    topDown(ee, e);
-  }
+
+  iterItems(ee, m);
 
   return EXIT_SUCCESS;
 }
